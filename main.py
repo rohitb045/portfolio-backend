dd874ab4e5b5ad1e9503535aac4807b4a611217e
rohitb045 <rohitbulbule07@gmail.com>
2026-04-14 13:10:49 +0530

    backend Email

--- main.py
+++ main.py
@@ -1,32 +1,67 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from pymongo import MongoClient
-from fastapi.middleware.cors import CORSMiddleware
+import smtplib
+from email.mime.text import MIMEText
+import os
 
 app = FastAPI()
 
-# ✅ CORS FIX (VERY IMPORTANT)
+# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # React frontend
-    allow_credentials=True,
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# MongoDB connection
-client = MongoClient("mongodb+srv://shubham:Shubham@cluster0.l6q3n6r.mongodb.net/?appName=Cluster0")
+# MongoDB
+client = MongoClient(os.getenv("MONGO_URL"))
 db = client["portfolio"]
 collection = db["leads"]
 
-# Schema
+# Your email (CHANGE THIS)
+EMAIL = "rohitbulbule07@gmail.com"
+PASSWORD = "zyhm qewz dheh upjw"
+
+
+# Form schema
 class Lead(BaseModel):
     name: str
     email: str
     mobile: str
 
+
+# Function to send email
+def send_email(data: Lead):
+    msg = MIMEText(
+        f"""
+New Resume Request:
+
+Name: {data.name}
+Email: {data.email}
+Mobile: {data.mobile}
+"""
+    )
+
+    msg["Subject"] = "New Portfolio Lead 🚀"
+    msg["From"] = EMAIL
+    msg["To"] = EMAIL
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(EMAIL, PASSWORD)
+    server.send_message(msg)
+    server.quit()
+
+
 # API
 @app.post("/submit")
-async def submit_data(data: Lead):
-    collection.insert_one(data.model_dump())
-    return {"message": "Data saved successfully"}
+async def submit(data: Lead):
+    collection.insert_one(data.dict())
+
+    # send email
+    send_email(data)
+
+    return {"message": "Saved + Email Sent"}
